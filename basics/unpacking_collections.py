8cff23bea5390b8866a5a82a13537291c69ddde4
nocsland <nocsland@gmail.com>
2022-05-05 20:50:31 +0400

    added some content

---
+++ basics/unpacking_collections.py
@@ -0,0 +1,11 @@
+# Распаковка коллекций
+collection = [25, "Иван", 85]
+# Трём переменным age, name и weight, присваиваются значения элементов списка с индексами 0, 1 и 2.
+age, name, weight = collection
+print(age, name, weight)
+
+# обмен переменных
+a = 1
+b = 2
+a, b = b, a
+print(b, a)

ba34d699d3e7bc84a046d7b8d65d36acc190f4f2
nocsland <nocsland@gmail.com>
2022-05-22 19:16:36 +0400

    added variant in q46

--- Q46/question_46.py
+++ Q46/question_46.py
@@ -1,9 +1,13 @@
 # 46. Проверьте, что в строке только числа
 #
-# Можно использовать isnumeric():
+# Можно использовать isnumeric() или isdigit():
 
 print('123a'.isnumeric())
 # => False
 
 print('123'.isnumeric())
 # => True
+
+print("123456".isdigit())
+
+print("123a123".isdigit())
